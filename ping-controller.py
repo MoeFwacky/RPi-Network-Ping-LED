8b30fbe23d8b746f8c1f3e48e0fa68ba71520471
Moe Fwacky <moe.fwacky@gmail.com>
2018-03-08 01:07:53 -0800

    Update ping-controller.py

--- ping-controller.py
+++ ping-controller.py
@@ -20,9 +20,9 @@ GPIO.setup(CNTRL_BCM, GPIO.OUT)
 CNTRL_LED = GPIO.PWM( CNTRL_BCM, 50)
 #CNTRL_LED.start(1)
 
-server = '192.168.1.140'
+server = '192.168.1.101'
 client = '192.168.1.102'
-controller = '192.168.1.123'
+controller = '192.168.1.103'
 multiplier = 7
 
 #start = time.time()
